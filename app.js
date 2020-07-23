85d733c4c2b29bca17520f4baa30259e847c1e9d
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-23 15:53:04 -0700

    add Constructors

--- app.js
+++ app.js
@@ -38,9 +38,104 @@ apartOne = {
  location: "long Island",   
  rooms: 3
 }
-apartOne = {
+apartTwo = {
  location: "brooklyn",   
- rooms: 4
+ color: "white"
+
 }
 
-apartTwo.__proto__=apartOne;
+apartTwo.__proto__ = apartOne;
+
+apartTwo.rooms = 4;
+console.log(apartTwo.rooms)
+console.log(apartOne.rooms)
+
+function User (name) {//constructer Function
+    // this
+    this.name = name;
+    // retun this
+}
+let adam = new User('Adam');
+let pete = new User('Pete');
+
+console.log(pete.name);
+
+console.log(adam.name);
+function NBAPlaeyer(name, team, threePtShooter) {
+    this.name = name;
+    this.team = team;
+    this.threePtShooter = threePtShooter;
+    this.intro = function() {
+        console.log ("hi may name is" +  this.name)
+    }
+}
+let steph = new NBAPlaeyer( "Steph Curry", "Warriors", true)
+console.log(steph);
+steph.intro()//call the this.intro functin above
+ 
+
+function User (pianist, musician){
+    this.pianist = pianist;
+    this.musician = musician;
+    this.myNameIs = function() {
+        console.log("hi, my name is Nick")
+    }
+}
+let nick = new User('Nick Phillips', "pianist", true);
+let john = new User( "John Lennon", "musician", true)
+console.log(nick)
+console.log(nick.name)
+nick.myNameIs()
+
+// make a class
+
+class Car  {
+    constructor( year, make, model, color) {
+        this.name = year;
+        this.make = make;
+        this.color = color;
+        this.model = model;
+    }
+    drive(){
+        console.log("Vroom");
+    }
+    intro(){
+        console.log('This is a ' + this.make + ' ' + this.model + " " + 'and goes');
+    }
+}
+
+let tesla = new Car('2020', 'Tesla','Model S', 'White');
+console.log(tesla);
+tesla.drive();
+tesla.intro();
+
+
+
+class GitProfile{
+    constructor( username, name, url) {
+        this.username = username;
+        this.name = name;
+        this.url = url;
+    }
+    intro(){
+        console.log(`My name is ${this.name} and my username is @${this.username}`)
+    }
+}
+fetch('https://api.github.com/users/maestronick1')
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    let gitUrl = data.url;
+    console.log(gitUrl);
+  let gitUserName = data.login;
+  console.log(gitUserName);
+  let gitName = data.name;
+
+  let nick = new GitProfile(gitUserName, gitName, gitUrl);
+  console.log(nick)
+
+})
+
+
